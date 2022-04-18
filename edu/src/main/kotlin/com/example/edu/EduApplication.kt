3973bf340ae9fbcc2752c8f56b5ca75a68239ed5
cwkim723 <jerry.pref@gmail.com>
2022-04-19 00:49:40 +0900

    feat: edu repo 생성

---
+++ edu/src/main/kotlin/com/example/edu/EduApplication.kt
@@ -0,0 +1,11 @@
+package com.example.edu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EduApplication
+
+fun main(args: Array<String>) {
+	runApplication<EduApplication>(*args)
+}
